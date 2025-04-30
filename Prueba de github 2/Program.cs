d8289b332f929afbb48fae3d46eaaa07776ce635
GerberCanahuiUMG <gerbercanahui39@gmail.com>
2025-04-29 23:21:35 -0600

    Agregar archivos de proyecto.

---
+++ Prueba de github 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba_de_github_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prueba de hit 2");
+        }
+    }
+}
